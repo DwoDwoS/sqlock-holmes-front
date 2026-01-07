6e50c24375b8ea77fa7af258faff5f9bef477a9e
Elouan <elouangauriaud@gmail.com>
2026-01-07 11:06:57 +0100

    add: vitest.config.ts to initiate tests in project

---
+++ vitest.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    exclude: ['node_modules', 'dist', 'coverage'],
+    passWithNoTests: false,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'json'],
+      reportsDirectory: 'coverage',
+      all: false,
+      statements: 80,
+      branches: 70,
+      functions: 75,
+      lines: 80,
+      exclude: [
+          'src/main.tsx',
+          'src/main.ts',
+          'src/**/*.d.ts',
+          'tests/**',
+          '**/*.css',
+          'src/assets/**',
+          'public/**'
+      ]
+    } as any
+  }
+});
